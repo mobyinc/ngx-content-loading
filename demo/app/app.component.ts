ed798d1ca5cf91d666c5264764ac0a4ccc7eb1bc
James Jacoby <james@mobyinc.com>
2023-11-16 15:31:12 -0800

    fix demo import, ng-package

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgxContentLoadingComponent } from '../../src';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: true
+    standalone: true,
+    imports: [NgxContentLoadingComponent]
 })
 export class AppComponent {
   title = 'app';
